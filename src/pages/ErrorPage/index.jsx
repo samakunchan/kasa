e2e8c1bc33eaa65a1e0054f6893dd2d617694957
samakunchan <badgehades@hotmail.com>
2024-03-06 17:59:40 +0100

    feat(page): error
    - Mise en place de la page d'érreur
    - Création d'un model pour les érreurs
    - Création d'un utilitaire pour les textes d'érreurs

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -1,3 +1,22 @@
-const ErrorPage = () => <div>Page d'erreur</div>;
+import { ErrorModel } from '../../core/models/error-model';
+import { Link } from 'react-router-dom';
+
+/**
+ * Créer la page d'érreur
+ * @param errorModel {ErrorModel}
+ * @return {JSX.Element}
+ * @constructor
+ */
+const ErrorPage = ({ errorModel = ErrorModel.notFound }) => {
+  return (
+    <main className='error'>
+      <h1>{errorModel.statusCode}</h1>
+      <p>{errorModel.message}</p>
+      <Link className='link' to={'/'}>
+        Retourner sur la page d'accueil
+      </Link>
+    </main>
+  );
+};
 
 export default ErrorPage;
