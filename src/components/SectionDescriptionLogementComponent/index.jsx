5cd1d025c59a7b7cd35535be28f12b590b6b93a6
samakunchan <badgehades@hotmail.com>
2024-03-11 08:33:23 +0100

    feat(responsive): detail page

--- src/components/SectionDescriptionLogementComponent/index.jsx
+++ src/components/SectionDescriptionLogementComponent/index.jsx
@@ -15,15 +15,17 @@ import PropTypes from 'prop-types';
 const SectionDescriptionLogementComponent = ({ logement }) => {
   return (
     <section className={'description-logement'}>
-      <div className='title-and-host block'>
-        <BlockTitleSubtitle title={logement.title} location={logement.location} />
-        <BlockHostComponent name={logement.host.name} picture={logement.host.picture} />
+      <div className='description-container'>
+        <div className='title-and-tags-disposition block'>
+          <BlockTitleSubtitle title={logement.title} location={logement.location} />
+          <BlockTagComponent tags={logement.tags} />
+        </div>
+        <div className='host-and-rating-disposition block'>
+          <BlockHostComponent name={logement.host.name} picture={logement.host.picture} />
+          <BlockRatingComponent rating={logement.rating} />
+        </div>
       </div>
-      <div className='tag-and-rating block'>
-        <BlockTagComponent tags={logement.tags} />
-        <BlockRatingComponent rating={logement.rating} />
-      </div>
-      <div className='accordions-disposition block'>
+      <div className='accordions-disposition'>
         <BlockAccordionComponent title={'Description'} content={logement.description} />
         <BlockAccordionComponent title={'Equipements'} content={logement.equipments} />
       </div>
