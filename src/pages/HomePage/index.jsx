53d1a0b1fe66679ea7ca2a975cfb81e96e17e994
samakunchan <badgehades@hotmail.com>
2024-03-11 05:59:44 +0100

    doc: composant
    - Documentation des composants

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,6 +2,11 @@ import './index.scss';
 import SectionGalleryComponent from '../../components/SectionGalleryComponent';
 import SectionTitleAndBackgroundComponent from '../../components/SectionTitleAndBackgroundComponent';
 
+/**
+ * Construit la Home Page
+ * @return {JSX.Element<HomePage>}
+ * @constructor
+ */
 const HomePage = () => (
   <div className='App'>
     <SectionTitleAndBackgroundComponent title={'Chez vous et partout ailleurs'} />
